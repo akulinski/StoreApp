cf6b47808391029ca321e744602e0d2f60b9b766
Albert <albert.kulinski@hotmail.com>
2018-04-09 21:19:01 +0200

    Created DataBase controller

--- src/sample/DbController.java
+++ src/sample/DbController.java
@@ -1,6 +1,21 @@
 package sample;
 
-public class DbController {
+import java.sql.*;
 
+public class DbController {
+    private Connection connection=null;
+
+    DbController()
+    {
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:test.db");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
 
 }
